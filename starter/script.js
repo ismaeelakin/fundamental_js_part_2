bc26e67ba50e29a29c3469627eb1cb1df82ded73
ismaeelakin <ismaeelakin@gmail.com>
2025-02-06 15:47:38 +0100

    Basic_Array_method

--- starter/script.js
+++ starter/script.js
@@ -105,7 +105,7 @@ console.log (age1, age2);
 // console.log (scoreDolphins, scoreKoalas);
 // checkWinner (scoreDolphins, scoreKoalas);
 
-
+/*
 // Storing and replacing data in Arrays
 const friend1 = 'Micheal';
 const friend2 = 'Steven';
@@ -142,3 +142,36 @@ const age3 = calcAge (years [2]);
 console.log (age1, age2, age3);
 const ages = [calcAge(years[0])]; calcAge(years [1]), calcAge(years [years.length -1]);
 console.log(ages);
+*/
+
+//Basic Array Operations
+
+const friends = ['Micheal, Steven, Peter'];
+
+// Add Element
+const newLength = friends.push('jay');
+console.log(friends);
+console.log(newLength);
+friends.unshift('john');
+console.log(friends);
+// Remove element
+friends.pop(); //Last
+const popped = friends.pop();
+console.log (popped);
+console.log (friends);
+
+friends.shift(); // First
+console.log(friends);
+
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'
+));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}
