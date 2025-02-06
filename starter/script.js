bfa751b86716181ed6f94f04e8a4a89c41ec4e18
ismaeelakin <ismaeelakin@gmail.com>
2025-02-06 10:19:17 +0100

    function_coding_challenge_test2

--- starter/script.js
+++ starter/script.js
@@ -82,8 +82,8 @@ console.log (calcAverage(3, 4, 5));
 
 
 //Test 1
-const scoreDolphins = calcAverage (44, 23, 71);
-const scoreKoalas = calcAverage (65, 54, 49);
+let scoreDolphins = calcAverage (44, 23, 71);
+let scoreKoalas = calcAverage (65, 54, 49);
 console.log (scoreDolphins, scoreKoalas);
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= 2 * avgKoalas) {
@@ -97,3 +97,10 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 }
 checkWinner (scoreDolphins, scoreKoalas);
 checkWinner (576, 111);
+
+
+// Test 2
+ scoreDolphins = calcAverage (85, 54, 21);
+ scoreKoalas = calcAverage  (23, 34, 27);
+console.log (scoreDolphins, scoreKoalas);
+checkWinner (scoreDolphins, scoreKoalas);
